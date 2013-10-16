6d1734ca60ec517bc2c185abf0aff48b9f503f1e
Tetoatom <yoshiaki.ariake@gmail.com>
2013-10-16 20:39:07 +0900

    Added snmp core class.

---
+++ iSnmpKit/iSKSnmpCore.h
@@ -0,0 +1,13 @@
+//
+//  iSKSnmpCore.h
+//  iSnmpKit
+//
+//  Created by 有明 義明 on 2013/10/16.
+//  Copyright (c) 2013年 Tetoatom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface iSKSnmpCore : NSObject
+
+@end
---
+++ iSnmpKit/iSKSnmpCore.m
@@ -0,0 +1,13 @@
+//
+//  iSKSnmpCore.m
+//  iSnmpKit
+//
+//  Created by 有明 義明 on 2013/10/16.
+//  Copyright (c) 2013年 Tetoatom. All rights reserved.
+//
+
+#import "iSKSnmpCore.h"
+
+@implementation iSKSnmpCore
+
+@end
